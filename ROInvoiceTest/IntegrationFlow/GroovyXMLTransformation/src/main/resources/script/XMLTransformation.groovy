fc78c74926fe67d955aa3fe9b7182df2de2fac27
root <root@ip-172-31-70-21.ec2.internal>
2023-04-10 17:11:37 +0000

    commit from environment

--- ROInvoiceTest/IntegrationFlow/GroovyXMLTransformation/src/main/resources/script/XMLTransformation.groovy
+++ ROInvoiceTest/IntegrationFlow/GroovyXMLTransformation/src/main/resources/script/XMLTransformation.groovy
@@ -3,7 +3,7 @@ import groovy.xml.MarkupBuilder
 
 import java.text.SimpleDateFormat
 
-//This is test rowl cpi git upload
+//This is test rowl cpi git upload modified in Cloud Integration Suite
 def Message processData(Message message) {
   Reader reader = message.getBody(Reader)
   def Order = new XmlSlurper().parse(reader)
